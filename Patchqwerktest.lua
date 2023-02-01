f3dba75c6fc526a177cf297bd65ffac217ca650e
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-02-01 04:01:37 -0700

    Update Patchqwerktest.lua
    Made things more complex

--- Patchqwerktest.lua
+++ Patchqwerktest.lua
@@ -1,4 +1,12 @@
-local npcId = 400012 --defines a variable npcId with a value of 400012.
+--The following is a sample script for creating a boss fight. The boss will use three abilities, and has various yell functions depending on its combat state.
+
+
+local npcId = 400012 --ID from creature_template
+
+local function OnSpawn(event, creature) --Creature yells upon spawning. I've also set this up to send a world message as this is tied to a world event I created. You can delete the SendWorldMessage line.
+	creature:SendUnitYell("Patchqwerk make Lich King proud! You die now!",0)  --0 indicates language. In this case, universal.
+	SendWorldMessage("The Lich King has sent forth his forces to attack the people of Azeroth. Quickly heroes, defend your cities!")
+end
 
 local function CastTrample(eventId, delay, calls, creature) --CastTrample, CastHatefulStrike, and CastGore are functions that cast spells on the creature's target (obtained through creature:GetVictim()). The spells cast are specified by their spell IDs (5568, 28308, and 48130 respectively).
     creature:CastSpell(creature:GetVictim(), 5568, true)
@@ -12,25 +20,33 @@ local function CastGore(eventId, delay, calls, creature)
     creature:CastSpell(creature:GetVictim(), 48130, true)
 end
 
-local function OnEnterCombat(event, creature, target) --OnEnterCombat is a function that is triggered when the creature enters combat, it sends a unit yell message with text "Tasty!", and registers the spells from the first 3 functions to be cast every 10, 15 and 20 seconds respectively.
-	creature:SendUnitYell("Tasty!", 0)
-	creature:RegisterEvent(CastTrample, 10000, 0)
-    creature:RegisterEvent(CastHatefulStrike, 15000, 0)
-    creature:RegisterEvent(CastGore, 20000, 0)
-	end
+local function OnEnterCombat(event, creature, target) --OnEnterCombat is a function that is triggered when the creature enters combat, it sends a unit yell message and registers the spells from the first 3 functions to be cast every 10, 15 and 20 seconds respectively.
+	local yellOptions = { "Patchqwerk huuuuungry!", "Time for a snack!", "You're mine now!", "You look delicious. Patchqwerk eat you now!", "I not eat in days, time to feast!", "Me smash and eat you now!", "Me so hungry, me eat anything... even you!" }
+	local randomIndex = math.random(1, 7) --7 = number of dialogue options
+	local selectedYell = yellOptions[randomIndex] --script pulls one dialogue option from above upon entering combat.
+	creature:SendUnitYell(selectedYell, 0)
+	creature:RegisterEvent(CastTrample, 10000, 0) --The 0 in creature:RegisterEvent(CastTrample, 10000, 0) is the number of times the event will be called. In this case, 0 means the event will be called an unlimited number of times until the creature is killed or leaves combat. If you set it to 1, for example, the event will only be called once and then stop.
+	creature:RegisterEvent(CastHatefulStrike, 15000, 0)
+	creature:RegisterEvent(CastGore, 20000, 0)
+end
 	
-local function OnLeaveCombat(event, creature) --OnLeaveCombat is a function that is triggered when the creature leaves combat, it sends a unit yell message with text "You not so tasty after all..." and removes all registered events.
-    creature:SendUnitYell("You not so tasty afterall...", 0)
-    creature:RemoveEvents()
+local function OnLeaveCombat(event, creature) --Stuff the creature will do upon leaving combat, in this case yell
+	local yellOptions = { "You not so tasty afterall...", "I'll be back for seconds!", "No more play? Too bad...", "Maybe next time you'll taste better!","Me still hungry, come back later!","You not enough food, me go find more!" }
+	local randomIndex = math.random(1, 6)
+	local selectedYell = yellOptions[randomIndex]
+	creature:SendUnitYell(selectedYell, 0)
+	creature:RemoveEvents()
 end
 
-local function OnDied(event, creature, killer) --OnDied is a function that is triggered when the creature dies. If the killer is a player, it sends a broadcast message to the player with the text "You killed <creature name>!". The function also removes all registered events.
-    if(killer:GetObjectType() == "Player") then
+local function OnDied(event, creature, killer) --OnDied is a function that is triggered when the creature dies. Creature sends Yell and broadcast message. If the killer is a player, it sends a broadcast message to the player with the text "You killed <creature name>!". The function also removes all registered events.
+    creature:SendUnitYell("Patchqwerk forget to chew...", 0)
+	if(killer:GetObjectType() == "Player") then
         killer:SendBroadcastMessage("You killed " ..creature:GetName().."!")
     end
     creature:RemoveEvents()
 end
 
-RegisterCreatureEvent(npcId, 1, OnEnterCombat) --RegisterCreatureEvent is a function that registers the event handlers OnEnterCombat, OnLeaveCombat, and OnDied for the specified creature npcId with events 1, 2, and 4 respectively.
+RegisterCreatureEvent(npcId, 1, OnEnterCombat) --RegisterCreatureEvent is a function that registers the event handlers OnEnterCombat, OnLeaveCombat, OnDied, and Onspawn for the specified creature npcId with events 1, 2, 4, and 5 respectively.
 RegisterCreatureEvent(npcId, 2, OnLeaveCombat)
 RegisterCreatureEvent(npcId, 4, OnDied)
+RegisterCreatureEvent(npcId, 5, OnSpawn)
